20e360b86e1e45d9d85e80f2d5f99703a4f3129a
Aayush Sthapit <aayushratnasthapit@gmail.com>
2022-04-12 07:59:50 +0545

    Replace JS based react-app with TS based react-app

---
+++ frontend/src/components/Todo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Component for Todo.
+function Todo() {
+    const [todos, setTodos] = React.useState(null);
+
+    React.useEffect(() => {
+
+    },[]);
+
+    return <div>Hello world</div>
+}
+
+export default Todo;
